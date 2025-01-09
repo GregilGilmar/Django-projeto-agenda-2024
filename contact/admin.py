91c53f799861431d7e6e445322de3f4cedd3bd07
GregilGilmar <gil128mar@gmail.com>
2025-01-09 20:08:38 -0300

    Configurandp Models

--- contact/admin.py
+++ contact/admin.py
@@ -9,7 +9,13 @@ class ContactAdmin(admin.ModelAdmin):
     ordering = '-id',
     # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
-    list_per_page = 1
+    # list_per_page = 1
     list_max_show_all = 200
     list_editable = 'last_name',
     list_display_links = 'id', 'first_name', 'phone',
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
